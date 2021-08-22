eaa0c76224259771bf00daa53bf670eda178fe47
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-22 17:20:13 -0300

    feat: entity address mapped for hsql

--- src/main/java/com/example/demo/model/Address.java
+++ src/main/java/com/example/demo/model/Address.java
@@ -1,10 +1,35 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "addresses")
 public class Address {
-	private int zip;
-	private String publicPlace;
-	private String district;
-	private String state;
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;	
+    
+    @Column(name = "zip")
+    @NotEmpty
+    private int zip;
+    
+    @Column(name = "publicPlace")
+    @NotEmpty
+    private String publicPlace;
+	
+    @Column(name = "district")
+    @NotEmpty
+    private String district;
+    
+    @Column(name = "state")
+    @NotEmpty
+    private String state;
 	
 	public Address(int zip, String publicPlace, String district, String state) {
 		super();
